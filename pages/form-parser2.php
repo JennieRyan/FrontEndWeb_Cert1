302921576edd9af34bad481677e239f697bacd21
Jennie Ryan <jennieryan@hotmail.com>
2016-05-09 12:13:10 -0700

    Add files via upload

---
+++ pages/form-parser2.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+Your name is: <?php echo $_POST['name']; ?><br />
+Your e-mail: <?php echo $_POST['email']; ?><br />
+Your phone is: <?php echo $_POST['phone']; ?><br />
+
+    
+<br />
+Comments:<br />
+<?php echo $_POST['comments']; ?>
+</body>
+</html>
