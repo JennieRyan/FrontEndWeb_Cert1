304121c2bfeec2797a25aeeb21f66c96b4ce3847
Jennie Ryan <jennieryan@hotmail.com>
2016-05-09 12:14:23 -0700

    Add files via upload

--- pages/form-parser2.php
+++ pages/form-parser2.php
@@ -3,10 +3,28 @@
 Your name is: <?php echo $_POST['name']; ?><br />
 Your e-mail: <?php echo $_POST['email']; ?><br />
 Your phone is: <?php echo $_POST['phone']; ?><br />
+Your web address is: <?php echo $_POST['url']; ?><br />
+  
+Your donation amount is: <?php echo $_POST['sponsorAmt']; ?>
+  <?php echo $_POST['customAmt']; ?>
+  <br />
+  
+The region you want to help is: <?php echo $_POST['region']; ?><br />
 
+Your newsletter preference is:
+
+    <?php 
+        if(!empty($_POST['newsletter'])) {
+            foreach($_POST['newsletter'] as $news) {
+                echo $news;
+            }
+        }
+    
+    ?>
+    
     
 <br />
 Comments:<br />
-<?php echo $_POST['comments']; ?>
+<?php echo $_POST['comment']; ?>
 </body>
 </html>
